671000c89ec483a69158886422758079b6b9e33e
alejandropascuale <apascuale@outlook.com>
2022-02-13 09:47:34 -0300

    first commit

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>This it a shit app</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +17,8 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  personal: {
+    color: 'black',
+    fontWeight: 'bold'
+  }
 });
