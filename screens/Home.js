c6386a36411b70e0aa8b27febc5ed41769778431
alejandropascuale <apascuale@outlook.com>
2022-02-14 17:26:41 -0300

    add styles and data for home component

---
+++ screens/Home.js
@@ -0,0 +1,111 @@
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import React, { useEffect } from 'react';
+import { StyleSheet, TouchableOpacity, View, Image } from 'react-native';
+import { FontAwesome, Entypo } from '@expo/vector-icons';
+import colors from '../colors';
+const catImageUrl = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/4d/Cat_November_2010-1a.jpg/767px-Cat_November_2010-1a.jpg'
+
+const Home = () => {
+
+    const navigation = useNavigation();
+
+    useEffect(() => {
+        navigation.setOptions({
+            headerLeft: () => (
+                <FontAwesome name='search' size={24} color={colors.gray} style={{ marginLeft: 15 }} />
+            ),
+            headerRight: () => (
+                <Image 
+                    source={{ uri: catImageUrl }}
+                    style={{
+                        width: 40,
+                        height: 40,
+                        marginRight: 15,
+                    }}
+                />
+            )
+        })
+    }, [navigation]);
+
+    return (
+        <View>
+            <TouchableOpacity 
+                onPress={() => NavigationContainer.navigate('Chat')}
+                style={styles.chatButton}
+                >
+                <Entypo name='chat' size={24} color={'lightGray'} />
+            </TouchableOpacity>
+        </View>
+    );
+}
+
+export default Home;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-end',
+        alignItems: 'flex-end',
+        backgroundColor: '#fff',
+    },
+    chatButton: {
+        backgroundColor: colors.primary,
+        height: 50,
+        width: 50,
+        borderRadius: 36,
+        alignItems: 'center',
+        justifyContent: 'center',
+        shadowColor: colors.primary,
+        shadowOffset: {
+            width: 0,
+            height: 2
+        },
+        shadowOpacity: .9,
+        shadowRadius: 8,
+        marginRight: 20,
+        marginBottom: 50,
+    },
+    title: {
+        fontSize: 36,
+        fontWeight: 'bold',
+        color: 'orange',
+        alignSelf: 'center',
+        paddingBottom: 24,
+    },
+    input: {
+        backgroundColor: '#F6F7FB',
+        height: 58,
+        marginBottom: 20,
+        fontSize: 16,
+        borderRadius: 10,
+        padding: 12,
+    },
+    backImage: {
+        width: '100%',
+        height: 340,
+        position: 'absolute',
+        top: 0,
+        resizeMode: 'cover',
+    },
+    whiteSheet: {
+        width: '100%',
+        height: '75%',
+        position: 'absolute',
+        bottom: 0,
+        backgroundColor: '#fff',
+        borderTopLeftRadius: 60,
+    },
+    form: {
+        flex: 1,
+        justifyContent: 'center',
+        marginHorizontal: 30,
+    },
+    button: {
+        backgroundColor: '#f57c00',
+        height: 58,
+        borderRadius: 10,
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginTop: 40,
+    }
+})
