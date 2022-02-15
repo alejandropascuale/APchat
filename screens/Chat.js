4bd1e69b209f2939e8fd3c8b871c7c9d1eabf3c9
alejandropascuale <apascuale@outlook.com>
2022-02-15 09:55:06 -0300

    add state and save messages in firebase

--- screens/Chat.js
+++ screens/Chat.js
@@ -9,7 +9,58 @@ import { AntDesign } from '@expo/vector-icons';
 import colors from '../colors';
 
 export default function Chat () {
+    const [messages, setMessages] = useState([]);
+    const navigation = useNavigation();
+
+    const onSignOut = () => {
+        signOut(auth).catch(error => console.log(error));
+    }
+
+    useLayoutEffect(() => {
+        navigation.setOptions({
+            headerRight: () => (
+                <TouchableOpacity style={{ marginRight: 10 }} onPress={onSignOut} >
+                    <AntDesign name='logout' size={24} color={colors.gray} style={{ marginRight: 10 }} />
+                </TouchableOpacity>
+            )
+        })
+    }, [navigation]);
+
+    useEffect(() => {
+        const colletionRef = collection(database, 'chats');
+        const q = query(colletionRef, orderBy('createdAt', 'desc'));
+        const unsubscribe = onSnapshot(q, snapshot => {
+            console.log(snapshot);
+            setMessages(
+                snapshot.docs.map(doc => ({
+                    _id: doc.id,
+                    createdAt: doc.data().createdAt.toDate(),
+                    text: doc.data().text,
+                    user: doc.data().user
+                }))
+            )
+        });
+        return () => unsubscribe();
+    }, [])
+
+    const onSend = useCallback((messages = []) => {
+        setMessages(previousMessages => GiftedChat.append(previousMessages, messages));
+        const {_id, createdAt, text, user} = messages[0];
+        addDoc(collection(database, 'chats'), {_id, createdAt, text, user});
+    }, [])
+
     return (
-        <GiftedChat />
+        <GiftedChat
+            messages={messages}
+            onSend={messages => onSend(messages)}
+            user={{
+                _id: auth?.currentUser?.email,
+                avatar: 'https://i.pravatar.cc/300'
+            }}
+            messagesContainerStyle={{
+                backgroundColor: '#fff'
+            }}
+            showAvatarForEveryMessage={true}
+        />
     )
 }
